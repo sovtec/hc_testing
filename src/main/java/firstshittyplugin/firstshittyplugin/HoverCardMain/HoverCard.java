135ecf3fc1e239edaf65e0a1582ee673f1f823c8
hrdu_ <ragnarbergseth@yahoo.no>
2022-06-23 12:29:04 +0200

    third commit, functional HoverCard v1

--- src/main/java/firstshittyplugin/firstshittyplugin/HoverCardMain/HoverCard.java
+++ src/main/java/firstshittyplugin/firstshittyplugin/HoverCardMain/HoverCard.java
@@ -4,19 +4,20 @@ import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class HoverCard {
+public class HoverCard implements Listener {
 
     @EventHandler
     private ItemStack getExampleItemStack() {
-        ItemStack itemStack == new ItemStack(Material.DIAMOND_SWORD);
-        ItemStack meta = itemStack.getItemMeta();
-        List<String> lore = new ArrayList<String>();
+        ItemStack itemStack = new ItemStack(Material.DIAMOND_SWORD);
+        ItemStack meta = (ItemStack) itemStack.getItemMeta();
+        List<String> lore = new ArrayList<>();
 
         ((ItemMeta) meta).setDisplayName(ChatColor.YELLOW + "Test");
         lore.add(ChatColor.RED + "SUPERCOOL dude.. or whatever lol");
@@ -25,7 +26,7 @@ public class HoverCard {
         ((ItemMeta) meta).addEnchant(Enchantment.DAMAGE_ALL, 5, true);
         ((ItemMeta) meta).addEnchant(Enchantment.FIRE_ASPECT, 2, true);
 
-        itemStack.setItemMeta(meta);
+        itemStack.setItemMeta((ItemMeta) meta);
         return itemStack;
 
     }
